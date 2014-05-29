7f92a7f9e13725dc48f0b9b78b4f969dcfe6e9f9
prstcsnpr <prstcsnpr@10.2.201.237>
2014-05-29 14:17:55 +0800

    理清client server的调用关系

--- client/www/js/index.js
+++ client/www/js/index.js
@@ -1,49 +1,118 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+/**
+ * 
  */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+PREFIX_URL = "http://localhost:8888"
 
-        console.log('Received Event: ' + id);
-    }
-};
+function getArgument() {
+	return JSON.parse(localStorage.argument);
+}
+
+function setArgument(argument) {
+	localStorage.argument = JSON.stringify(argument);
+}
+
+$(document).on("pageinit", "#schoolinfos", function() {
+	
+	$(document).on("tap", ".schoolinfos-schoolitem", function() {
+		setArgument({"schoolID":$(this).find("input").val()});
+	});
+	
+	$(document).on("pagebeforeshow", "#schoolinfos", function() {
+		loadSchoolInfos();
+	});
+	
+	function loadSchoolInfos() {
+		$.ajax({
+			url:PREFIX_URL+"/schoolinfos",
+			dataType:"json",
+			success:function(data) {
+				list = new Array();
+				for (var i = 0; i < data.result.length; i++) {
+					school_info = {};
+					school_info.id = data.result[i].id;
+					school_info.name = data.result[i].name;
+					school_info.sat = data.result[i].sat;
+					school_info.toefl = data.result[i].toefl;
+					school_info.englishName = data.result[i].englishName;
+					list[i] = school_info;
+				}
+				var result = {"list":list};
+				$("#schoolinfos-content").html(Handlebars.compile($("#schoolinfos-content-template").html())(result));
+				$("#schoolinfos-content").listview("refresh");
+			}
+		});
+	}
+});
+
+$(document).on("pageinit", "#schoolinfo", function() {
+	
+	$(document).on("pagebeforeshow", "#schoolinfo", function() {
+		var schoolID = getArgument().schoolID;
+		loadSchoolInfo(schoolID);
+	});
+	
+	function loadSchoolInfo(schoolID) {
+		$.ajax({
+			url:PREFIX_URL+"/schoolinfo/"+schoolID,
+			dataType:"json",
+			success:function(data) {
+				result = {};
+				$("#schoolinfo-schoolname").text(data.result.schoolName);
+				$("#schoolinfo-schoolprofile").text(data.result.schoolProfile);
+				$("#schoolinfo-admissionstatus").text(data.result.admissionStatus);
+			}
+		});
+	}
+});
+
+$(document).on("pageinit", "#candidatesrecommendation", function() {
+	
+	$(document).on("pagebeforeshow", "#candidatesrecommendation", function() {
+		var sat = getArgument().sat;
+		var toefl = getArgument().toefl;
+		var discipline = getArgument().discipline;
+		loadCandidatesRecommendation(sat, toefl, discipline);
+	});
+
+	
+	$(document).on("tap", ".candidatesrecommendation-schoolitem", function() {
+		setArgument({"schoolID":$(this).find("input").val()});
+	});
+	
+	function loadCandidatesRecommendation(sat, toefl, discipline) {
+		$.ajax({
+			url:PREFIX_URL+"/candidatesrecommendation",
+			dataType:"json",
+			type:"POST",
+			data:{"sat":sat, "toefl":toefl, "discipline":discipline},
+			success:function(data) {
+				list = new Array();
+				for (var i = 0; i < data.result.length; i++) {
+					school_info = {};
+					school_info.id = data.result[i].id;
+					school_info.name = data.result[i].name;
+					school_info.rate = data.result[i].rate;
+					list[i] = school_info;
+				}
+				var result = {"list":list};
+				$("#candidatesrecommendation-content").html(Handlebars.compile($("#candidatesrecommendation-content-template").html())(result));
+				$("#candidatesrecommendation-content").listview("refresh");
+			}
+		});
+	}
+});
+
+$(document).on("pageinit", "#candidatestools", function() {
+	
+	$(document).on("tap", "#candidatestools－calculate", function() {
+		var sat = $("#candidatestools－sat").val();
+		var toefl = $("#candidatestools－toefl").val();
+		var discipline = $("#candidatestools－discipline").val();
+		setArgument({
+			"sat":sat,
+			"toefl":toefl,
+			"discipline":discipline,
+		});
+	});
+});
--- server/src/candidates_guide.py
+++ server/src/candidates_guide.py
@@ -1,13 +1,151 @@
+# -*- coding: utf-8 -*-
+
+
+import json
+import string
 import tornado.ioloop
 import tornado.web
 
+
 class MainHandler(tornado.web.RequestHandler):
+    
     def get(self):
         self.write("Hello, world")
+        
+
+class CandidatesRecommendationHandler(tornado.web.RequestHandler):
+    
+    def post(self):
+        sat = self.get_argument("sat")
+        toefl = self.get_argument("toefl")
+        discipline = self.get_argument("discipline")
+        school_list = self.__get_school_list()
+        json_result = self.__generate_json(school_list)
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.write(json_result)
+        
+    def __generate_json(self, result):
+        json_result = {}
+        json_result["result"] = result
+        json_result["error"] = 0
+        json_result["description"] = "No error"
+        return json.dumps(json_result)
+    
+    def __get_school_list(self):
+        list = []
+        school_info1 = {}
+        school_info1["id"] = 1
+        school_info1["name"] = "哈佛大学"
+        school_info1["rate"] = "80%"
+        school_info2 = {}
+        school_info2["id"] = 2
+        school_info2["name"] = "麻省理工学院"
+        school_info2["rate"] = "70%"
+        school_info3 = {}
+        school_info3["id"] = 3
+        school_info3["name"] = "吉林大学"
+        school_info3["rate"] = "69%"
+        list.append(school_info1)
+        list.append(school_info2)
+        list.append(school_info3)
+        return list
+        
+class SchoolInfosHandler(tornado.web.RequestHandler):
+    
+    def get(self):
+        school_list = self.__get_school_list()
+        json_result = self.__generate_json(school_list)
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.write(json_result)
+        
+    def __generate_json(self, result):
+        json_result = {}
+        json_result["result"] = result
+        json_result["error"] = 0
+        json_result["description"] = "No error"
+        return json.dumps(json_result)
+        
+    def __get_school_list(self):
+        list = []
+        school_info1 = {}
+        school_info1["id"] = 1
+        school_info1["name"] = "哈佛大学"
+        school_info1["englishName"] = "HARVARD"
+        school_info1["sat"] = 100
+        school_info1["toefl"] = 110
+        school_info2 = {}
+        school_info2["id"] = 2
+        school_info2["name"] = "麻省理工学院"
+        school_info2["englishName"] = "MIT"
+        school_info2["sat"] = 100
+        school_info2["toefl"] = 100
+        school_info3 = {}
+        school_info3["id"] = 3
+        school_info3["name"] = "吉林大学"
+        school_info3["englishName"] = "JLU"
+        school_info3["sat"] = 90
+        school_info3["toefl"] = 90
+        list.append(school_info1)
+        list.append(school_info2)
+        list.append(school_info3)
+        return list
+        
+        
+class SchoolInfoHandler(tornado.web.RequestHandler):
+    
+    def get(self, school_id):
+        school_info = self.__get_school_info(string.atoi(school_id))
+        json_result = self.__generate_json(school_info)
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.write(json_result)
+        
+    def __generate_json(self, result):
+        json_result = {}
+        json_result["result"] = result
+        json_result["error"] = 0
+        json_result["description"] = "No error"
+        return json.dumps(json_result)
+        
+    def __get_school_info(self, id):
+        if 1 == id:
+            return self.__get_1_school_info()
+        elif 2 == id:
+            return self.__get_2_school_info()
+        elif 3 == id:
+            return self.__get_3_school_info()
+        
+    def __get_1_school_info(self):
+        school_info = {}
+        school_info["schoolID"] = 1
+        school_info["schoolName"] = "哈佛大学"
+        school_info["schoolProfile"] = "一大堆情况介绍"
+        school_info["admissionStatus"] = "一大堆文字"
+        return school_info
+    
+    def __get_2_school_info(self):
+        school_info = {}
+        school_info["schoolID"] = 2
+        school_info["schoolName"] = "麻省理工学院"
+        school_info["schoolProfile"] = "两大堆情况介绍"
+        school_info["admissionStatus"] = "两大堆文字"
+        return school_info
+    
+    def __get_3_school_info(self):
+        school_info = {}
+        school_info["schoolID"] = 3
+        school_info["schoolName"] = "吉林大学"
+        school_info["schoolProfile"] = "三大堆情况介绍"
+        school_info["admissionStatus"] = "三大堆文字"
+        return school_info
+    
 
 application = tornado.web.Application([
     (r"/", MainHandler),
+    (r"/schoolinfo/([0-9]+)", SchoolInfoHandler),
+    (r"/schoolinfos", SchoolInfosHandler),
+    (r"/candidatesrecommendation", CandidatesRecommendationHandler),
 ])
+
 
 if __name__ == "__main__":
     application.listen(8888)
